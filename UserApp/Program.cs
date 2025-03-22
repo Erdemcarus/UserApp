595b251a0875eafe19945f6eb13df9b7ad582571
Erdem Carus <erdemcarus2@icloud.com>
2025-03-22 12:35:22 +0300

    Add project files.

---
+++ UserApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UserApp
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User kullanıcı = new User();
+
+            kullanıcı.Adsoyad = "Erdem Carus";
+            kullanıcı.Yas = 25 ;
+            kullanıcı.eMail = "erdem@gmail.com";
+            kullanıcı.BilgileriGoster();
+
+        }
+    }
+
+}
+    
---
+++ UserApp/User.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserApp
+{
+   public  class User
+    {
+        private string adSoyad;
+        private int yas;
+
+        public string eMail { get; set; }
+
+       public string Adsoyad {
+            
+            get{ return adSoyad; }
+            set { adSoyad = value; } }
+       
+        public int Yas
+        {
+            get { return yas; }
+
+            set { yas = value; }
+
+        }
+
+        public void BilgileriGoster()
+        {
+
+            Console.WriteLine("Kullanıcının adı ve soyadı: "+Adsoyad);
+            Console.WriteLine("Kullanıcının yaşı: "+Yas);
+            Console.WriteLine("Kullanıcının email adresi: "+eMail);
+
+
+        }
+
+    }
+}
